a3a3aaebc172aa054dee9e1b3bad49f9d7d88827
KacperTabakaHG <kacper.tabaka@student.hogent.be>
2025-01-27 13:53:07 +0100

    Added go project

---
+++ backend/Linkify.MainApi/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module dev.azure.com/kacpertabaka/_git/Graduaatproef-Linkify
+
+go 1.23.5
+
+require github.com/go-chi/chi/v5 v5.2.0
