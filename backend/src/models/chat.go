cacdd45a1b8a1a0fa5ac56be4c5e53f509485c51
Kacper Tabaka <kacper.tabaka@student.hogent.be>
2025-02-23 00:21:23 +0100

    Implemented chat struct

---
+++ backend/src/models/chat.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"errors"
+	"github.com/google/uuid"
+)
+
+type chat struct {
+	id           uuid.UUID
+	participants []uuid.UUID
+	admins       []uuid.UUID
+}
+
+func NewChat(id uuid.UUID, participants, admins []uuid.UUID) (*chat, []error) {
+
+	var errs []error
+
+	if id == uuid.Nil {
+		id = uuid.New()
+	}
+	if participants == nil {
+		errs = append(errs, errors.New("participants cannot be nil"))
+	}
+	if admins == nil {
+		errs = append(errs, errors.New("admins cannot be nil"))
+	}
+	if len(errs) > 0 {
+		return nil, errs
+	}
+	return &chat{id: id, participants: participants, admins: admins}, nil
+}
