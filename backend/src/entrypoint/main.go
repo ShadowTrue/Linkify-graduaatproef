77b0f766e9b4b6198241c67067a49e9b133925e4
ShadowTrue <kt.kk48@gmail.com>
2025-05-03 21:51:05 +0200

    Created a method to create a connection to mongoDb server

--- backend/src/entrypoint/main.go
+++ backend/src/entrypoint/main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	//TODO "net/http"
 )
 
 func main() {
 	fmt.Println("Hello World")
+
+	db := createDbConnection()
+
+	db.Collection("Users")
 	
-	
+}
+
+
+func createDbConnection() *mongo.Database{
+	client, err := mongo.Connect(options.Client().ApplyURI("mongodb://localhost:27017/"))
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	//this line handle the timeout, 
+	// after an operation doesnt finish withing selected timeframe it cancel it.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+	err = client.Ping(ctx,nil)
+
+	if err != nil{
+		log.Fatal("Could not ping the database", err)
+	}
+
+	db :=  client.Database("LinkifyDb")
+	log.Println("Connection to database successfully established")
+	return db
 }
